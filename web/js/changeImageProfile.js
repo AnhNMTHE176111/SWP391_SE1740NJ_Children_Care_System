54d59b0611f269c3374569f501a1a9ce1232d8ad
AnhNMTHE176111 <AnhNMTHE176111@fpt.edu.vn>
2023-09-16 01:37:02 +0700

    doing pop-up...

---
+++ web/js/changeImageProfile.js
@@ -0,0 +1,11 @@
+function previewProfileImage(input) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+
+        reader.onloadend = function (e) {
+            document.querySelector('#image-user').src = reader.result;
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
