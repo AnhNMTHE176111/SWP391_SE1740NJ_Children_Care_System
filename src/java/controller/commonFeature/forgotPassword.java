7ff979e30ead4270d9653cb021558ec7fa39a175
anhlthe172031 <letienanh1908@gmail,com>
2023-10-16 17:36:29 +0700

    Tienanh commit forgot pass

--- src/java/controller/commonFeature/forgotPassword.java
+++ src/java/controller/commonFeature/forgotPassword.java
@@ -9,7 +9,13 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.Random;
 
-import jakarta.servlet.RequestDispatcher;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
