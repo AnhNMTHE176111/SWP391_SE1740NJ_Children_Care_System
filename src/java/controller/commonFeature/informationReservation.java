d139078d87c7185cb3d7fd1a99f61ca5e7ac570d
AnhLTHE172031-LeTienAnh <letienanh1908@gmail,com>
2023-10-24 03:23:14 +0700

    AnhLTHE172031-LeTienAnh commit myReservation

--- src/java/controller/commonFeature/informationReservation.java
+++ src/java/controller/commonFeature/informationReservation.java
@@ -2,10 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package controller.commonFeature;
 
-import DAO.DAOBooking;
-import DAO.DAOCustomer;
 import DAO.DAOUser;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,48 +14,42 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.util.List;
-import model.Booking;
-import model.Customer;
 import model.User;
 
 /**
  *
  * @author ASUS
  */
-@WebServlet(name = "myReservation", urlPatterns = {"/my"})
-public class myReservation extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+@WebServlet(name="informationReservation", urlPatterns={"/information"})
+public class informationReservation extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet myReservation</title>");
+            out.println("<title>Servlet informationReservation</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet myReservation at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet informationReservation at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -64,35 +57,15 @@ public class myReservation extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         HttpSession session = request.getSession();
         DAOUser userDao = new DAOUser();
-        DAOBooking bookingDao = new DAOBooking();
-        DAOCustomer cusDao = new DAOCustomer();
         User user = (User) session.getAttribute("user");
-        Customer cusId = cusDao.getCusIdByUserId(user.getUserId());
-         String pageIndexStr = request.getParameter("pageIndex");
-        if (pageIndexStr == null) {
-            pageIndexStr = "1";
-        }
-        int pageIndex = Integer.parseInt(pageIndexStr);
+         request.getRequestDispatcher("reservationInformation.jsp").forward(request, response);
+    } 
 
-
-        int count = bookingDao.getTotalCusReservation(cusId.getId());
-        int endPage = count / 2;
-        if (count % 2 != 0) {
-            endPage++;
-        }
-        List<Booking> customerReservation = bookingDao.getListCusReservation(cusId.getId(), pageIndex);
-        request.setAttribute("endP", endPage);
-        session.setAttribute("customerReservation", customerReservation);
-        session.setAttribute("cusName", user.getFirstName() + " " + user.getLastName());
-        request.getRequestDispatcher("myReservation.jsp").forward(request, response);
-    }
-
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -100,13 +73,12 @@ public class myReservation extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
